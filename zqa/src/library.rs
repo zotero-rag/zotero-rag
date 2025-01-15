12bb30b6d2c4c5d5b3bc8889438f65bc7d4ace7c
Rahul Yedida <yrahul3910@gmail.com>
2025-01-14 22:01:26 -0500

    Add docs

--- zqa/src/library.rs
+++ zqa/src/library.rs
@@ -5,6 +5,10 @@ use std::env;
 use std::error::Error;
 use std::path::PathBuf;
 
+/// Gets the Zotero library path. Works on Linux, macOS, and Windows systems.
+///
+/// Returns None if either the OS is not one of the above, or if we could not
+/// get the directory.
 fn get_lib_path() -> Option<PathBuf> {
     match env::consts::OS {
         "linux" | "macos" | "windows" => {
@@ -14,12 +18,14 @@ fn get_lib_path() -> Option<PathBuf> {
     }
 }
 
+/// Metadata for items in the Zotero library.
 pub struct ZoteroItemMetadata {
     library_key: String,
     title: String,
     paper_abstract: String,
 }
 
+/// A general error struct for Zotero library parsing.
 #[derive(Debug)]
 pub struct LibraryParsingError {
     message: String,
@@ -33,6 +39,9 @@ impl fmt::Display for LibraryParsingError {
 
 impl Error for LibraryParsingError {}
 
+/// Parses the Zotero library. If successful, returns a list of metadata for each item.
+/// The SQL query essentially combines the titles and abstracts for each paper into a
+/// single row. This is done using GROUP BY on the keys, and MAX to get the field itself.
 pub fn parse_library() -> Result<Vec<ZoteroItemMetadata>, Box<dyn Error>> {
     if let Some(path) = get_lib_path() {
         let conn = Connection::open(&path)?;
