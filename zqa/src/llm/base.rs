4738bd1c61badc6593a3da3539802ffb3596c45d
Rahul Yedida <yrahul3910@gmail.com>
2025-03-09 01:33:32 -0500

    docs: add docstrings

--- zqa/src/llm/base.rs
+++ zqa/src/llm/base.rs
@@ -1,11 +1,15 @@
 use super::errors::LLMError;
 
+/// Backend-independent struct to represent what's sent to the APIs.
+/// Implementations of ApiClient will all need at least this info.
 #[derive(Debug, Clone)]
 pub struct UserMessage {
     chat_history: Vec<String>,
     message: String,
 }
 
+/// Response info from APIs that we are at least interested in.
+/// Implementations of ApiClient should return this.
 #[derive(Debug)]
 pub struct ApiResponse {
     pub content: String,
@@ -13,11 +17,14 @@ pub struct ApiResponse {
     pub output_tokens: u32,
 }
 
+/// A struct to hold secrets from .env
 pub struct Secrets {
     endpoint: String,
     api_key: String,
 }
 
+/// Trait that clients should implement. This will be fleshed out
+/// more as the project progresses.
 #[allow(async_fn_in_trait)]
 pub trait ApiClient {
     async fn send_message(&self, message: &UserMessage) -> Result<ApiResponse, LLMError>;
