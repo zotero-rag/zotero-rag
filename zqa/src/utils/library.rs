0b7c0183c4149d195c6db9f6510b2794233db8e7
Rahul Yedida <yrahul3910@gmail.com>
2025-05-08 02:59:40 -0400

    feat(zqa): WIP: add basic integration with pdftools and rag

--- zqa/src/utils/library.rs
+++ zqa/src/utils/library.rs
@@ -5,6 +5,8 @@ use std::env;
 use std::error::Error;
 use std::path::PathBuf;
 
+use pdftools::parse::extract_text;
+
 /// Gets the Zotero library path. Works on Linux, macOS, and Windows systems.
 ///
 /// Returns None if either the OS is not one of the above, or if we could not
@@ -19,6 +21,7 @@ fn get_lib_path() -> Option<PathBuf> {
 }
 
 /// Metadata for items in the Zotero library.
+#[derive(Clone)]
 pub struct ZoteroItemMetadata {
     pub library_key: String,
     pub title: String,
@@ -27,10 +30,18 @@ pub struct ZoteroItemMetadata {
     pub file_path: PathBuf,
 }
 
+/// A Zotero library item. Includes full-text from parsing PDFs when they exist.
+/// TODO: For now, we assume the PDF text always exists. This should be generalized later.
+pub struct ZoteroItem {
+    pub metadata: ZoteroItemMetadata,
+    pub text: String,
+}
+
 /// A general error struct for Zotero library parsing.
 #[derive(Clone, Debug)]
 pub enum LibraryParsingError {
     LibNotFoundError,
+    PdfParsingError(String),
 }
 
 impl From<rusqlite::Error> for LibraryParsingError {
@@ -39,18 +50,25 @@ impl From<rusqlite::Error> for LibraryParsingError {
     }
 }
 
+impl From<Box<dyn Error>> for LibraryParsingError {
+    fn from(value: Box<dyn Error>) -> Self {
+        LibraryParsingError::PdfParsingError(value.to_string())
+    }
+}
+
 impl fmt::Display for LibraryParsingError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             LibraryParsingError::LibNotFoundError => write!(f, "Library not found!"),
+            LibraryParsingError::PdfParsingError(m) => write!(f, "PDF parsing error: {}", m),
         }
     }
 }
 
 impl Error for LibraryParsingError {}
 
-/// Parses the Zotero library. If successful, returns a list of metadata for each item.
-pub fn parse_library() -> Result<Vec<ZoteroItemMetadata>, LibraryParsingError> {
+/// Parses the Zotero library metadata. If successful, returns a list of metadata for each item.
+pub fn parse_library_metadata() -> Result<Vec<ZoteroItemMetadata>, LibraryParsingError> {
     if let Some(path) = get_lib_path() {
         let conn = Connection::open(path.join("zotero.sqlite"))?;
 
@@ -76,7 +94,7 @@ pub fn parse_library() -> Result<Vec<ZoteroItemMetadata>, LibraryParsingError> {
             .query_map([], |row| {
                 let res_path: String = row.get(4)?;
                 let split_idx = res_path.find(':').unwrap_or(0);
-                let filename = res_path.split_at(split_idx).1;
+                let filename = res_path.split_at(split_idx + 1).1;
                 let lib_key: String = row.get(0)?;
 
                 Ok(ZoteroItemMetadata {
@@ -96,6 +114,55 @@ pub fn parse_library() -> Result<Vec<ZoteroItemMetadata>, LibraryParsingError> {
     }
 }
 
+/// Parses the Zotero library, also parsing PDF files if they exist on disk. If not, we currently
+/// discard those items. TODO: Update this logic later on.
+pub fn parse_library() -> Result<Vec<ZoteroItem>, LibraryParsingError> {
+    let metadata = parse_library_metadata()?;
+
+    let mut failed_count = 0;
+    let items = metadata
+        .iter()
+        .filter_map(|m| {
+            let path_str = match m.file_path.to_str() {
+                Some(p) => p,
+                None => {
+                    failed_count += 1;
+                    log::warn!(
+                        "Skipping item with invalid UTF-8 in path: {:?}",
+                        m.library_key
+                    );
+                    return None;
+                }
+            };
+
+            match extract_text(path_str) {
+                Ok(text) => Some(ZoteroItem {
+                    metadata: m.clone(),
+                    text,
+                }),
+                Err(e) => {
+                    failed_count += 1;
+                    log::warn!(
+                        "Failed to parse PDF for item {} with path {}: {}",
+                        m.library_key,
+                        path_str,
+                        e
+                    );
+                    None
+                }
+            }
+        })
+        .collect::<Vec<_>>();
+
+    if failed_count > 0 {
+        log::warn!("Failed to parse {} PDF files", failed_count);
+    }
+
+    log::info!("Parsed {} items from library.", items.len());
+
+    Ok(items)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -107,7 +174,7 @@ mod tests {
             return;
         }
 
-        let library_items = parse_library();
+        let library_items = parse_library_metadata();
 
         assert!(library_items.is_ok());
         let items = library_items.unwrap();
