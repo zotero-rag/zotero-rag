c818b6f3bc8b8acf68f2713164b2cc0d959da77f
Rahul Yedida <yrahul3910@gmail.com>
2025-06-18 00:49:44 -0400

    refactor(rag): use enum for API providers, only register required embedding function

--- rag/src/llm/base.rs
+++ rag/src/llm/base.rs
@@ -1,7 +1,7 @@
 use super::errors::LLMError;
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct ChatHistoryItem {
     pub role: String,
     pub content: String,
@@ -9,7 +9,7 @@ pub struct ChatHistoryItem {
 
 /// A user-facing struct that does not carry API-specific information. Clients should
 /// convert from this to native message types.
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct UserMessage {
     pub chat_history: Vec<ChatHistoryItem>,
     pub message: String,
@@ -17,7 +17,7 @@ pub struct UserMessage {
 
 /// A user-facing struct representing API responses, containing only information users
 /// would be interested in.
-#[derive(Debug, Clone, Serialize, Deserialize, Default)]
+#[derive(Clone, Debug, Default, Deserialize, Serialize)]
 pub struct ApiResponse {
     pub content: String,
     pub input_tokens: u32,
@@ -27,4 +27,27 @@ pub struct ApiResponse {
 #[allow(async_fn_in_trait)]
 pub trait ApiClient {
     async fn send_message(&self, message: &UserMessage) -> Result<ApiResponse, LLMError>;
+}
+
+#[derive(Clone, Debug)]
+pub enum ModelProviders {
+    OpenAI,
+    Anthropic,
+}
+
+impl ModelProviders {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            ModelProviders::OpenAI => "openai",
+            ModelProviders::Anthropic => "anthropic",
+        }
+    }
+
+    pub fn contains(provider: &str) -> bool {
+        return [
+            ModelProviders::OpenAI.as_str(),
+            ModelProviders::Anthropic.as_str(),
+        ]
+        .contains(&provider);
+    }
 }
