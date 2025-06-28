0e92e57a8f522cf7719eadd3f873bdcfb1d64ae5
Rahul Yedida <yrahul3910@gmail.com>
2025-06-28 00:27:54 -0400

    feat: add output box to tui

--- zqa/src/main.rs
+++ zqa/src/main.rs
@@ -1,6 +1,9 @@
+use dotenv::dotenv;
 use zqa::ui::app::App;
 
 pub fn main() {
+    dotenv().ok();
+
     let mut terminal = ratatui::init();
     let _ = App::default().run(&mut terminal);
     ratatui::restore();
