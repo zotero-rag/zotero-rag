07c9f2231f4ecfc7facf12be4aef2aec83ba7a22
Rahul Yedida <yrahul3910@gmail.com>
2025-06-28 00:44:20 -0400

    ci: fix clippy errors

--- zqa/src/ui/commands.rs
+++ zqa/src/ui/commands.rs
@@ -15,7 +15,7 @@ pub fn handle_process_command(app: &mut App) {
 
     if let Err(parse_err) = item_metadata {
         app.output_lines
-            .push(format!("Could not parse metadata: {parse_err}").into());
+            .push(format!("Could not parse metadata: {parse_err}"));
         app.line_styles.push(Style::default().fg(Color::Yellow));
 
         return;
@@ -24,12 +24,9 @@ pub fn handle_process_command(app: &mut App) {
     let item_metadata = item_metadata.unwrap();
     let metadata_length = item_metadata.len();
     if metadata_length >= WARNING_THRESHOLD {
-        app.output_lines.push(
-            format!(
-                "Your library has {metadata_length} items. Parsing may take a while. Continue?"
-            )
-            .into(),
-        );
+        app.output_lines.push(format!(
+            "Your library has {metadata_length} items. Parsing may take a while. Continue?"
+        ));
         app.line_styles.push(Style::default().fg(Color::Yellow));
     }
 }
