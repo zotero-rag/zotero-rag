b585451db2e27635250da1dce25d14d4092874fc
Rahul Yedida <yrahul3910@gmail.com>
2025-07-05 00:09:15 -0400

    feat(zqa): add non-tui cli

---
+++ zqa/src/cli/app.rs
@@ -0,0 +1,78 @@
+use lancedb::embeddings::EmbeddingDefinition;
+use rag::vector::lance::create_initial_table;
+
+use crate::{library_to_arrow, utils::library::parse_library_metadata};
+use std::io::{self, Write};
+
+pub async fn process() {
+    const WARNING_THRESHOLD: usize = 100;
+    let item_metadata = parse_library_metadata(None, None);
+
+    if let Err(parse_err) = item_metadata {
+        println!("Could not parse library metadata: {parse_err}");
+        return;
+    }
+
+    let item_metadata = item_metadata.unwrap();
+    let metadata_length = item_metadata.len();
+    if metadata_length >= WARNING_THRESHOLD {
+        println!("Your library has {metadata_length} items. Parsing may take a while. Continue?");
+        print!("(/process) >>> ");
+
+        let mut option = String::new();
+        io::stdin()
+            .read_line(&mut option)
+            .expect("Failed to read input.");
+
+        if ["n", "no"].contains(&option.as_str()) {
+            return;
+        }
+    }
+
+    let batch_iter = library_to_arrow(None, None).expect("Failed to parse library");
+    let db = create_initial_table(
+        batch_iter,
+        EmbeddingDefinition::new(
+            "data",             // source column
+            "anthropic",        // embedding name, either "openai" or "anthropic"
+            Some("embeddings"), // dest column
+        ),
+    )
+    .await;
+
+    if db.is_ok() {
+        println!("Successfully parsed library!");
+    } else {
+        println!("Parsing library failed: {}", db.err().unwrap().to_string())
+    }
+}
+
+pub async fn cli() {
+    loop {
+        print!(">>> ");
+        let _ = io::stdout().flush();
+
+        let mut command = String::new();
+        io::stdin()
+            .read_line(&mut command)
+            .expect("Failed to read command.");
+
+        let command = command.trim();
+
+        match command {
+            "" => {}
+            "/help" => {
+                println!("Available commands:\n");
+                println!("/help\t\tShow this help message");
+                println!("/process\tPre-process Zotero library. Use to update the database.");
+                println!();
+            }
+            "/process" => {
+                process().await;
+            }
+            invalid => {
+                println!("Invalid command: {invalid}");
+            }
+        }
+    }
+}
--- zqa/src/main.rs
+++ zqa/src/main.rs
@@ -1,10 +1,28 @@
+use clap::Parser;
 use dotenv::dotenv;
+use zqa::cli::app::cli;
 use zqa::ui::app::App;
 
-pub fn main() {
-    dotenv().ok();
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Whether to use the tui interface
+    #[arg(long, default_value_t = false)]
+    tui: bool,
+}
 
-    let mut terminal = ratatui::init();
-    let _ = App::default().run(&mut terminal);
-    ratatui::restore();
+#[tokio::main]
+pub async fn main() {
+    dotenv().ok();
+    let args = Args::parse();
+
+    if args.tui {
+        let mut terminal = ratatui::init();
+        let _ = App::default().run(&mut terminal);
+        ratatui::restore();
+
+        return;
+    }
+
+    cli().await;
 }
