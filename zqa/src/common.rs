3e56dfccd878117753c36651539c0ef793fcdf87
Rahul Yedida <yrahul3910@gmail.com>
2025-07-08 01:22:13 -0400

    fix(zqa): fix deadlock, add log level arg, improved logging

--- zqa/src/common.rs
+++ zqa/src/common.rs
@@ -7,6 +7,10 @@ pub struct Args {
     #[arg(long, default_value_t = false)]
     pub tui: bool,
 
+    /// Log level
+    #[arg(long, default_value_t = String::from("off"))]
+    pub log_level: String,
+
     /// Choice of embedding provider. Valid options are 'anthropic', 'openai', and 'voyageai'. Note
     /// that 'anthropic' uses the OpenAI embedding model, so will require `OPENAI_API_KEY` to be
     /// set. 'voyageai' will require `VOYAGE_AI_API_KEY` to be set.
