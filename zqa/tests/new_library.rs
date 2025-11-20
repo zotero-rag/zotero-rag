482fec066243241d2d4185536e4f80fd31e977cf
Rahul Yedida <yrahul3910@gmail.com>
2025-11-20 07:47:53 +0000

    fix: fix remaining errors

--- zqa/tests/new_library.rs
+++ zqa/tests/new_library.rs
@@ -2,9 +2,11 @@ use arrow_array::RecordBatchIterator;
 use dotenv::dotenv;
 use lancedb::embeddings::EmbeddingDefinition;
 use log::LevelFilter;
+use rag::constants::{DEFAULT_MAX_CONCURRENT_REQUESTS, DEFAULT_MAX_RETRIES, VOYAGE_EMBEDDING_DIM};
 use rag::vector::lance::insert_records;
 use std::env;
 use zqa::common::setup_logger;
+use zqa::config::{AnthropicConfig, Config, VoyageAIConfig};
 use zqa::full_library_to_arrow;
 
 #[tokio::test]
@@ -17,7 +19,30 @@ async fn test_integration_works() {
         return;
     }
 
-    let record_batch = full_library_to_arrow("voyageai", None, None).await;
+    let config = Config {
+        model_provider: "anthropic".into(),
+        embedding_provider: "voyageai".into(),
+        reranker_provider: "voyageai".into(),
+        max_concurrent_requests: DEFAULT_MAX_CONCURRENT_REQUESTS,
+        max_retries: DEFAULT_MAX_RETRIES,
+        gemini: None,
+        openai: None,
+        cohere: None,
+        openrouter: None,
+        anthropic: Some(AnthropicConfig {
+            model: "claude-sonnet-4-5".into(),
+            api_key: env::var("ANTHROPIC_API_KEY").unwrap().into(),
+            max_tokens: 8192,
+        }),
+        voyageai: Some(VoyageAIConfig {
+            reranker: Some("rerank-2.5".into()),
+            embedding_model: Some("voyage-3-large".into()),
+            embedding_dims: Some(VOYAGE_EMBEDDING_DIM as usize),
+            api_key: Some(env::var("VOYAGE_AI_API_KEY").unwrap().into()),
+        }),
+    };
+
+    let record_batch = full_library_to_arrow(&config, None, None).await;
     assert!(record_batch.is_ok());
 
     let record_batch = record_batch.unwrap();
@@ -28,6 +53,7 @@ async fn test_integration_works() {
     let db = insert_records(
         batch_iter,
         None,
+        &config.get_embedding_config().unwrap(),
         EmbeddingDefinition::new(
             "pdf_text",         // source column
             "openai",           // embedding name, either "openai" or "anthropic"
