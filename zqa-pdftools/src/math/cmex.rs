1da20778d02d83a56af4c4d71e78e17d0944e14f
Rahul Yedida <yrahul3910@gmail.com>
2026-01-30 23:00:23 -0500

    fix: undo changes to pdftools

--- zqa-pdftools/src/math/cmex.rs
+++ zqa-pdftools/src/math/cmex.rs
@@ -1,7 +1,9 @@
-pub(crate) fn from_cmex(ch: u8) -> String {
+use std::borrow::Cow;
+
+pub(crate) fn from_cmex(ch: u8) -> Cow<'static, str> {
     match ch {
-        88 => "\\sum".to_string(), // X
-        90 => "\\int".to_string(), // Z
-        _ => char::from(ch).to_string(),
+        b'X' => Cow::Borrowed("\\sum"),
+        b'Z' => Cow::Borrowed("\\int"),
+        _ => Cow::Owned(char::from(ch).to_string()),
     }
 }
