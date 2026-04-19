50d42ec11f00f593c5d8052c5024465b445da913
Rahul Yedida <yrahul3910@gmail.com>
2026-04-19 00:30:26 -0400

    fix: use async_trait, move LanceDB-specific arg to struct

--- zqa-rag/src/vector/backends/backend.rs
+++ zqa-rag/src/vector/backends/backend.rs
@@ -1,6 +1,6 @@
 //! Traits for vector database backends.
 
-use std::fmt::Display;
+use async_trait::async_trait;
 
 use crate::providers::ProviderId;
 
@@ -18,16 +18,17 @@ pub trait VectorBackendRegistrar: VectorBackend + Send + Sync {
 }
 
 /// A vector database backend.
-pub trait VectorBackend {
+#[async_trait]
+pub trait VectorBackend: Send + Sync {
     /// The record type for the backend.
-    type Record;
+    type Record: Send;
     /// The error type for the backend.
-    type Error;
+    type Error: Send;
     /// The configuration type for the backend. A backend may not have a config at all, in which
     /// case the [`Config`] type should be `()`.
-    type Config;
+    type Config: Send + Sync;
     /// The connection type for the backend.
-    type Connection;
+    type Connection: Send + Sync;
 
     /// Returns the *base* path for the DB. This can be path on the local disk for LanceDB's
     /// database, a connection URL, etc.
@@ -36,7 +37,7 @@ pub trait VectorBackend {
 
     /// Whether the path specified by [`get_db_path`] exists.
     #[must_use]
-    fn db_exists(&self) -> impl Future<Output = bool> + Send + '_;
+    async fn db_exists(&self) -> bool;
 
     /// Create indices for the database. The details of the type of index are left to the trait
     /// implementer. If the index exists, then it is updated instead.
@@ -45,18 +46,14 @@ pub trait VectorBackend {
     ///
     /// * `text_col` - The name of the text column to index.
     /// * `embedding_col` - The name of the embedding column to index.
-    fn create_or_update_indices<'a>(
-        &'a self,
-        text_col: &'a str,
-        embedding_col: &'a str,
-    ) -> impl Future<Output = Result<(), Self::Error>> + Send + 'a;
+    async fn create_or_update_indices(
+        &self,
+        text_col: &str,
+        embedding_col: &str,
+    ) -> Result<(), Self::Error>;
 
-    /// Connect to the database using the given configuration.
-    ///
-    /// # Arguments
-    ///
-    /// * `config` - The configuration to use for connecting to the database.
-    fn connect(&self) -> impl Future<Output = Result<Self::Connection, Self::Error>> + Send + '_;
+    /// Connect to the database.
+    async fn connect(&self) -> Result<Self::Connection, Self::Error>;
 
     /// Delete rows from the database based on the given column and keys.
     ///
@@ -65,11 +62,7 @@ pub trait VectorBackend {
     /// * `col` - The name of the column to delete rows from.
     /// * `keys` - The keys of the rows to delete. The values must correspond to values in the
     ///   `col` column.
-    fn delete_rows<'a>(
-        &'a self,
-        col: &'a str,
-        keys: &'a [impl AsRef<str> + Send + Sync],
-    ) -> impl Future<Output = Result<(), Self::Error>> + Send + 'a;
+    async fn delete_rows(&self, col: &str, keys: &[String]) -> Result<(), Self::Error>;
 
     /// Deduplicate rows in the database based on the given column and key.
     ///
@@ -81,11 +74,7 @@ pub trait VectorBackend {
     /// # Returns
     ///
     /// The number of rows deleted.
-    fn dedup_rows<'a>(
-        &'a self,
-        by: &'a str,
-        key: &'a str,
-    ) -> impl Future<Output = Result<usize, Self::Error>> + Send + 'a;
+    async fn dedup_rows(&self, by: &str, key: &str) -> Result<usize, Self::Error>;
 
     /// Get all items from the database.
     ///
@@ -96,10 +85,7 @@ pub trait VectorBackend {
     /// # Returns
     ///
     /// A vector of all items in the database.
-    fn get_items<'a>(
-        &'a self,
-        columns: &'a [impl AsRef<str> + Send + Sync + Display],
-    ) -> impl Future<Output = Result<Vec<Self::Record>, Self::Error>> + Send + 'a;
+    async fn get_items(&self, columns: &[String]) -> Result<Vec<Self::Record>, Self::Error>;
 
     /// Search for items in the database using a vector query.
     ///
@@ -111,11 +97,11 @@ pub trait VectorBackend {
     /// # Returns
     ///
     /// A vector of items that match the query.
-    fn vector_search(
+    async fn vector_search(
         &self,
         query: String,
         limit: usize,
-    ) -> impl Future<Output = Result<Vec<Self::Record>, Self::Error>> + Send + '_;
+    ) -> Result<Vec<Self::Record>, Self::Error>;
 
     /// Search for an item in the database by its key.
     ///
@@ -127,11 +113,11 @@ pub trait VectorBackend {
     /// # Returns
     ///
     /// The item that matches the key, if one exists.
-    fn search_by_key<'a>(
-        &'a self,
-        key_col: &'a str,
-        key: &'a str,
-    ) -> impl Future<Output = Result<Option<Self::Record>, Self::Error>> + Send + 'a;
+    async fn search_by_key(
+        &self,
+        key_col: &str,
+        key: &str,
+    ) -> Result<Option<Self::Record>, Self::Error>;
 
     /// Search for items in the database by a column value.
     ///
@@ -143,11 +129,11 @@ pub trait VectorBackend {
     /// # Returns
     ///
     /// A vector of items that match the column value.
-    fn search_by_column<'a>(
-        &'a self,
-        col: &'a str,
-        values: &'a [impl AsRef<str> + Send + Sync + Display],
-    ) -> impl Future<Output = Result<Vec<Self::Record>, Self::Error>> + Send + 'a;
+    async fn search_by_column(
+        &self,
+        col: &str,
+        values: &[String],
+    ) -> Result<Vec<Self::Record>, Self::Error>;
 
     /// Insert items into the database.
     ///
@@ -156,15 +142,13 @@ pub trait VectorBackend {
     /// * `items` - The items to insert.
     /// * `merge_on` - `None` if you want to create or overwrite the current database; otherwise, a
     ///   reference to an array of keys to merge on.
-    /// * `source_col` - The name of the column in `items` that contains the source document text.
     ///
     /// # Returns
     ///
     /// `Ok(())` if the items were inserted successfully, or an error otherwise.
-    fn insert_items<'a>(
-        &'a self,
+    async fn insert_items(
+        &self,
         items: Vec<Self::Record>,
-        merge_on: Option<&'a [&str]>,
-        source_col: &'a str,
-    ) -> impl Future<Output = Result<(), Self::Error>> + Send + 'a;
+        merge_on: Option<&[&str]>,
+    ) -> Result<(), Self::Error>;
 }
